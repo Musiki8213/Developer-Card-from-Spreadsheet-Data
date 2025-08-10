fc9a9e9bd30a0bb5934aa27b2102d9cbb053e6e4
Musiki8213 <sithomolamusiki@gmail.com>
2025-08-10 20:56:02 +0200

    added short descriptions

--- App.js
+++ App.js
@@ -1,67 +1,64 @@
 const developer = {
-    username : "polyglotPro",
-    yearsExperience : 7,
-    isActive : true,
-    pictureUrl : "https://api.dicebear.com/9.x/adventurer/svg?seed=Riley",
-    email : "pro@example.com",
-    phone : "456-789-0124",
-    skills : "Go; Rust; Python; JS",
-    bio : "A versatile programmer with 7 years of experience, polyglotPro is fluent in multiple languages, including Go, Rust, and Python, tackling a wide range of programming challenges."
-    
-}
+    username: "polyglotPro",
+    yearsExperience: 7,
+    isActive: true,
+    pictureUrl: "https://api.dicebear.com/9.x/adventurer/svg?seed=Riley",
+    email: "pro@example.com",
+    phone: "456-789-0124",
+    skills: "Go; Rust; Python; JS",
+    bio: "A versatile programmer with 7 years of experience, polyglotPro is fluent in multiple languages, including Go, Rust, and Python, tackling a wide range of programming challenges."
+};
 
-///Card Div
+// Card Div
 const cardDiv = document.createElement("div");
 document.body.appendChild(cardDiv);
 cardDiv.id = "cardDiv";
 
-//Picture
+// Picture
 const picture = document.createElement("img");
 picture.src = developer.pictureUrl;
 cardDiv.appendChild(picture);
 picture.id = "imgID";
 
-
-//User Name
+// User Name
 const uName = document.createElement("h2");
 uName.textContent = developer.username;
 cardDiv.appendChild(uName);
 uName.id = "userNameID";
 
-//EXPERIENCE
+// Experience
 const experience = document.createElement("p");
-experience.textContent = developer.yearsExperience + " Years";
+experience.textContent = `Experience: ${developer.yearsExperience} Years`;
 cardDiv.appendChild(experience);
 experience.id = "experienceID";
 
-//Active
+// Active Status
 const active = document.createElement("p");
-active.textContent = developer.isActive;
+active.textContent = `Status: ${developer.isActive ? "Active" : "Inactive"}`;
 cardDiv.appendChild(active);
 active.id = "activeID";
 
-
-//Email
+// Email
 const mail = document.createElement("a");
-mail.href = developer.email;
-mail.textContent = developer.email;
+mail.href = `mailto:${developer.email}`;
+mail.textContent = `Email: ${developer.email}`;
 cardDiv.appendChild(mail);
 mail.id = "mailID";
 
-//Phone
+// Phone
 const phoneNumber = document.createElement("p");
-phoneNumber.textContent = developer.phone;
+phoneNumber.textContent = `Phone: ${developer.phone}`;
 cardDiv.appendChild(phoneNumber);
 phoneNumber.id = "phoneID";
 
-//Skills
+// Skills
 const skillss = document.createElement("p");
-skillss.textContent = developer.skills;
+skillss.textContent = `Skills: ${developer.skills}`;
 cardDiv.appendChild(skillss);
 skillss.id = "skillID";
 
-//Bio
+// Bio
 const bioo = document.createElement("p");
-bioo.textContent = developer.bio;
+bioo.textContent = `Bio: ${developer.bio}`;
 cardDiv.appendChild(bioo);
 bioo.id = "bioID";
